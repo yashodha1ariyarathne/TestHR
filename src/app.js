7f31c85a2570eec8fef32e6a6f8592d511a440ee
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-07-14 17:10:47 +0530

    input validation

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json({
     limit: '50mb'                   //Request size - 50MB
 }));
 
+
 //Handle CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
