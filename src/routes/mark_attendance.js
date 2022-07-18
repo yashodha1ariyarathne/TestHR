e5905fa6eef8070f744b18f0e1a096a6ab8ecc1c
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-07-18 14:40:57 +0530

    changed status codes

--- src/routes/mark_attendance.js
+++ src/routes/mark_attendance.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 var md5 = require('md5');
 
-router.get('/attendance', async(req, res,) => {
+router.get('/attendance', async(req, res,next) => {
 
   var id;
   var markAtt;
@@ -23,10 +23,10 @@ router.get('/attendance', async(req, res,) => {
   
 
   if(!username || !password || !status) 
-    return res.status(500).send("Please fill all required fields");
+    return res.status(400).send("Please fill all required fields");
 
   if (statusTypes.includes(statusLowCase) === false) 
-    return res.status(500).send("Invalied status type");
+    return res.status(400).send("Invalied status type");
 
   try {
 
@@ -34,25 +34,25 @@ router.get('/attendance', async(req, res,) => {
 
     if(!result.length)
 
-      return res.status(500).send("Incorrect Username or Password");
+      return res.status(400).send("Incorrect Username or Password");
 
               
     id = result[0].emp_id;
 
-    var valiedAttendance = await global.db.query('SELECT * FROM attendance WHERE date=CURRENT_DATE AND  emp_id=? AND status=?',[id,statusLowCase]);
+    var valiedAttendance = await global.db.query('SELECT * FROM attendanceN WHERE date=CURRENT_DATE AND  emp_id=? AND status=?',[id,statusLowCase]);
 
     if(!valiedAttendance.length){
       
       markAtt = await global.db.query('INSERT INTO attendance (emp_id,status,date,time,comment) VALUES(?,?,SYSDATE(),SYSDATE(),?)',[id,statusLowCase,comment]);
-      return res.status(200).send("Marked successfully");
+      return res.status(201).send("Marked successfully");
     }
       
-    res.status(200).send("Already marked"); 
+    res.status(400).send("Already marked"); 
   }
     
-  catch (error) {
+  catch (e) {
 
-    res.status(500).send(error);
+    next (new AppError(AppError.types.systemError, 0, e, 200, 500));
    
   }
 
