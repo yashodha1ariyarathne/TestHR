9028a5d89182a026657fc7a3d29cf7b5b9b428c0
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-07-25 11:32:36 +0530

    modified leave_req.js file

--- src/routes/leave_req.js
+++ src/routes/leave_req.js
@@ -8,23 +8,35 @@ router.get('/leave', async(req, res,next) => {
 
     let empId = req.body.empId;
     let reason = req.body.reason;
-    let dateOfLeaveRequired = req.body.date_of_leave_required;
-    let numberOfDaysOfLeaveRequired = req.body.number_of_days_of_leave_required;
+    let dateOfLeaveRequired = req.body.dateOfLeaveRequired;
+    let numberOfDaysOfLeaveRequired = req.body.numberOfDaysOfLeaveRequired;
+    let timeForHalfday=req.body.timeForHalfday;
     let leaveType = req.body.type;
 
     let valiedLeaveType = ['casual','medical','halfday'];
 
-    let leaveTypeLowCase = leaveType.toLowerCase();
-
-
-    //Checking whether mandatory data has been entered.
-    if(!empId || !dateOfLeaveRequired || !numberOfDaysOfLeaveRequired || !leaveType) 
-        return res.status(400).send("Please fill all required fields");
-
     
+    //Checking whether mandatory data has been entered.
+
+    if(!empId || !dateOfLeaveRequired || !leaveType) 
+        return res.status(400).send("Please fill all required fields");
+    
+    let leaveTypeLowCase = leaveType.toLowerCase();
+    
+    if(leaveTypeLowCase==='halfday'){
+        if(!timeForHalfday) 
+            return res.status(400).send("Please fill all required fields");
+    }
+    else{
+        if(!numberOfDaysOfLeaveRequired) 
+            return res.status(400).send("Please fill all required fields");
+    }       
+    
+
     //Checking that 'leave type' is entered correctly.
     if (valiedLeaveType.includes(leaveTypeLowCase) === false) 
-    return res.status(400).send("Invalied leave type");
+        return res.status(400).send("Invalied leave type");
+
     
     try {
 
@@ -38,20 +50,21 @@ router.get('/leave', async(req, res,next) => {
 
 
         //To find the total number of holidays available in the year for the employee's employee type and leave type
-        let result2 = await  global.db.query('SELECT * FROM availableleave  WHERE empTypeId = ? and leaveType = ?',[empTypeId,leaveTypeLowCase]);
-        let numberOfAvailabLeleave= result2[0].NumberOfLeaves
+        let resultAvalable = await  global.db.query('SELECT * FROM availableleave  WHERE empTypeId = ? and leaveType = ?',[empTypeId,leaveTypeLowCase]);
+        let numberOfAvailabLeleave= resultAvalable[0].NumberOfLeaves
         
 
         //To find out the total number of leaves taken by the respective employee during the year by employee type and leave type
-        let result3 = await  global.db.query('SELECT COUNT(emp_id) as numberOfLeaveTaken  FROM leavetaken  WHERE emp_id = ? AND leaveType = ?',[empId,leaveTypeLowCase]);
-        let numberOfLeaveTaken= result3[0].numberOfLeaveTaken  
+        let resultTaken = await  global.db.query('SELECT SUM(numberOfDaysOfLeave) as numberOfLeaveTaken  FROM leavetaken  WHERE emp_id = ? AND leaveType = ?',[empId,leaveTypeLowCase]);
+        let numberOfLeaveTaken= resultTaken[0].numberOfLeaveTaken
+            
 
         //Checking whether the employee has taken the total number of leaves available in the year for the employee type and leave type
         if(numberOfAvailabLeleave <= numberOfLeaveTaken)
-            return res.status(400).send("Sorry! All your leave will be taken for this leave type");
+            return res.status(400).send("Sorry, you've already taken all those leave-type leave.");
             
         //If everything is OK, enter the data into the database.
-        await global.db.query('INSERT INTO leaverequests (emp_id,reason,dateOfLeaveRequired,numberOfDaysOfLeaveRequired) VALUES(?,?,?,?)',[empId,reason,dateOfLeaveRequired,numberOfDaysOfLeaveRequired]);
+        await global.db.query('INSERT INTO leaverequests (emp_id,reason,dateOfLeaveRequired,numberOfDaysOfLeaveRequired,timeForHalfday) VALUES(?,?,?,?,?)',[empId,reason,dateOfLeaveRequired,numberOfDaysOfLeaveRequired,timeForHalfday]);
         res.status(201).send("Leave requests sent successfully");
         
     } 
