127943585b1c500cd8c438be5f8a4b91499f0528
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-09-21 17:24:46 +0530

    checking if the token has expired(in isAuthenticated method

--- ui/src/app/services/auth.service.ts
+++ ui/src/app/services/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AppService } from '../app.service';
 import { HttpClientModule } from "@angular/common/http";
-// import { AuthInterceptorService } from ".././auth-interceptor.service";
+
+import jwt_decode from 'jwt-decode';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,29 +12,49 @@ import { HttpClientModule } from "@angular/common/http";
 export class AuthService {
   
   constructor(
-  
     public jwtHelper: JwtHelperService,
     public appService: AppService,
-    
-    // public authinterceptorservice:AuthInterceptorService
+   
     ) { }
 
 
-    public isAuthenticated(): boolean {
-    
-      let token = localStorage.getItem('token');
-      
-      if (token) {
-        return true;
+  private getDecodedToken(token: any){
+      try {
+        return jwt_decode(token);
+      } catch(Error) {
+        return null;
       }
-  
+    }
+    
+  private setUser(token: any) {
+
+    let tokenInfo= this.getDecodedToken(token) as any;
+    this.appService.empId = tokenInfo.payload.empId;
+    this.appService.empTypeId = tokenInfo.payload.empTypeId;
+    
+  } 
+
+  public isAuthenticated(): boolean {
+    
+     // Check whether the token is expired and return
+    let token = localStorage.getItem('token')as any;
+   
+    if (!this.jwtHelper.isTokenExpired(token)) {
+      this.setUser(token);
+      return true;
+    }
+    else{
+      localStorage.removeItem("token")
       return false;
     }
-
+    
   }
 
+}
 
 export function tokenGetter() {
-  return localStorage.getItem('token');
+  
+  return localStorage.getItem("token")
 }
+
 
