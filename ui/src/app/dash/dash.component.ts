b49f0f14b1a544aca2094ce62e4038334839cbaa
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-09-23 13:12:56 +0530

    modified dash.component.ts and dash component.html

--- ui/src/app/dash/dash.component.ts
+++ ui/src/app/dash/dash.component.ts
@@ -16,14 +16,6 @@ export class DashComponent {
 
     empTypeId=this.appService.empTypeId;
 
-    isAuthenticatedUserType () : boolean {
-      if (this.empTypeId > 2) 
-        return true;
-
-      return false;
-    };
-  
-
   logout() {
     localStorage.removeItem('token');
   }
