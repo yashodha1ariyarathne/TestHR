5ebf003a8e51e4887db259c290e67d7c3dcb30ab
yashodha1ariyarathne <thamarakumari1980413@gmail.com>
2022-10-13 18:19:58 +0530

    modified mark_attendance route

--- server/src/routes/mark_attendance.js
+++ server/src/routes/mark_attendance.js
@@ -9,96 +9,156 @@ var moment = require('moment');
 
 router.post('/attendance' ,async(req, res,next) => {
   
-  var markAtt;
   var statusTypes={in:'in',out:'out'};
 
-  var  id = res.locals.empId;
-  var typeId=res.locals.empTyepeId;
-  var status   = req.body.status;
-  var comment  = req.body.comment;
-
+  var id = res.locals.empId;
+  var empTypeId = res.locals.empTypeId;
+  var status    = req.body.status;
+  var comment   = req.body.comment;
 
   var statusLowCase =status.toLowerCase();
 
+  var tFormat = 'hh:mm:ss';
+  // var time = moment(),
+  var time = moment('09:05:00', tFormat),
+  beforeTime = moment('09:00:00', tFormat),
+  afterTime = moment('09:15:00', tFormat);
 
-  try {
+  var startDate = moment("2022-10-10").format('YYYY-MM-DD');
+  var endDate = moment("2022-11-10").format('YYYY-MM-DD');
 
-    //-------------------Checking whether mandatory data has been entered.----------------------//
+  var leaveType ='halfday';
+  var numberOfDaysOfLeave = 1;
+
+  try { 
+
+    //-----------Checking whether mandatory data has been entered.----------------------//
     if(!status)
       return res.status(400).json("Please fill all required fields");
 
-
     //------------Checking that 'Status' is entered correctly.----------------------//
     if (!Object.values(statusTypes).includes(statusLowCase)) 
-    return res.status(400).json("Invalied status type");
+      return res.status(400).json("Invalied status type");
 
+    //-------------Checking if date related data is already entered.----------------------//
+    if(await allreadyMarked()){
+      return res.status(400).json("Already marked");
+    }
+     
+    //-------------Checking late attendance-------------------------//
+    if(statusLowCase === statusTypes.in){
 
-    //----------------------Checking if date related data is already entered.----------------------//
-    let valiedAttendance1 = await global.db.query('SELECT * FROM attendance WHERE date=CURRENT_DATE AND  emp_id=? AND status=?',[id,statusLowCase]);
-    
-    if(valiedAttendance1.length === 1)
-      return res.status(400).json("Already marked"); 
-    
+      if(withingLatePeriod()){
 
-    //----------------------checking late attendance-------------------------//
-    if (statusLowCase === statusTypes.in){
-      
-      var tFormat = 'hh:mm:ss';
-      // var dFormat = 'YYYY-MM-DD';
+        if(await markLateAttendance()){
 
-      var time = moment(),
-      // var time = moment('09:05:00', tFormat),
-      beforeTime = moment('09:00:00', tFormat),
-      afterTime = moment('09:15:00', tFormat);
-
-      var startDate = moment("2022-10-11").format('YYYY-MM-DD');
-      var endDate = moment('2022-11-10').format('YYYY-MM-DD');
-
-      // console.log(startDate,endDate)
-      
-      if(time > afterTime){
-        return res.status(400).json("You cannot mark your attendance");
+          if(await checkingForHalfdays()){
+            return res.status(400).json("You cannot mark your attendance");
+          } 
+          await global.db.query('INSERT INTO leavetaken (emp_id,leaveType,numberOfDaysOfLeave) VALUES(?,?,?)',[id,leaveType,numberOfDaysOfLeave]);
+          return res.status(400).json("Today is marked as a half-day leave.");
+        }
       }
-  
+      
+      if(lateAttendance()){
+        return res.status(400).json("You cannot mark your attendance");
 
-      if (time.isBetween(beforeTime, afterTime)) {
-        // let resultAttendanceForMonth = await  global.db.query('SELECT COUNT(*) as numberOfLateAttendance from attendance WHERE  emp_id =? AND date BETWEEN "2022-10-11" AND "2022-11-10" AND time BETWEEN "09:00:00" AND "09:15:00"',[id,startDate,endDate]);
-        let resultAttendanceForMonth = await  global.db.query('SELECT COUNT(*) as numberOfLateAttendance from attendance WHERE  emp_id =? AND date BETWEEN ? AND ?  AND time BETWEEN "09:00:00" AND "09:15:00"',[id,startDate,endDate]);
-        let numberOfLateAttendance= resultAttendanceForMonth[0].numberOfLateAttendance
-        console.log(resultAttendanceForMonth)
-
-        if(numberOfLateAttendance >= 4)
-          return res.status(400).json("You have delayed marking 'in' for 4 days this month.");
         
-      } 
+      }
+
+    }
+    else{
+      //----------------------If 'Out' is entered, checking whether 'In' is entered for that date.----------------------//
+      if(validOut()){
+        return res.status(400).json("You cannot mark 'out' without marking 'in'");
+      }
     }
     
-
-      
-    //----------------------If 'Out' is entered, checking whether 'In' is entered for that date.----------------------//
-    if (statusLowCase === statusTypes.out){
-
-      let valiedAttendance2 = await global.db.query('SELECT * FROM attendance WHERE date=CURRENT_DATE AND  emp_id=? AND status="in"',[id]);
-        
-      if(!valiedAttendance2.length)
-        return res.status(400).json("You cannot mark 'out' without marking 'in'");
-
-    }
 
     //----------------------If everything is OK, enter the data into the database.----------------------//
-    markAtt = await global.db.query('INSERT INTO attendance (emp_id,status,date,time,comment) VALUES(?,?,SYSDATE(),SYSDATE(),?)',[id,statusLowCase,comment]);
+    await global.db.query('INSERT INTO attendance (emp_id,status,date,time,comment) VALUES(?,?,SYSDATE(),SYSDATE(),?)',[id,statusLowCase,comment]);
     res.status(201).json("Marked successfully");
 
-  }
-    
+       
+  } 
+
   catch (error) {
-
     next (new AppError(AppError.types.systemError, 0, error, 200, 500));
-   
+    
+  }
+ 
+
+  async function allreadyMarked (){
+    let valiedAttendance1 = await global.db.query('SELECT * FROM attendance WHERE date=CURRENT_DATE AND  emp_id=? AND status=?',[id,statusLowCase]);
+    if(valiedAttendance1.length === 1){
+      return true;
+    }
+
+    return false;
+     
+  } 
+
+
+  function withingLatePeriod(){
+    if(time.isBetween(beforeTime, afterTime)){
+      return true;
+    }
+    return false; 
   }
 
+
+  function lateAttendance(){
+    if(time > afterTime){
+      return true;
+    }
+    return false;
+  }
+
+
+  async function markLateAttendance(){
+    let resultAttendanceForMonth = await  global.db.query('SELECT COUNT(*) as numberOfLateAttendance from attendance WHERE  emp_id =? AND date BETWEEN ? AND ?  AND time BETWEEN "09:00:00" AND "09:15:00"',[id,startDate,endDate]);
+    let numberOfLateAttendance= resultAttendanceForMonth[0].numberOfLateAttendance
+    
+    if(numberOfLateAttendance >= 4){
+      return true;
+    }
+    return false;
+      
+  }
+
+
+  async function checkingForHalfdays(){
+   
+    //----------To find the total number of halfdays available in the year for the employee's employee type.------------------//
+    let resultAvalable = await  global.db.query('SELECT * FROM availableleave  WHERE empTypeId = ? and leaveType = ?',[empTypeId,leaveType]);
+    let numberOfAvailabLeleave= resultAvalable[0].NumberOfLeaves
+    
+
+    //--------To find out the total number of halfdays taken by the respective employee during the year by employee type.---------------//
+    let resultTaken = await  global.db.query('SELECT SUM(numberOfDaysOfLeave) as numberOfLeaveTaken  FROM leavetaken  WHERE emp_id = ? AND leaveType = ?',[id,leaveType]);
+    let numberOfLeaveTaken= resultTaken[0].numberOfLeaveTaken
+        
+
+    //---------Checking whether the employee has taken the total number of halfdays available in the year for the employee type.---------//
+    if(numberOfAvailabLeleave <= numberOfLeaveTaken){
+      return true;
+    } 
   
-  
+    return false;
+      
+  }
+
+
+  async function validOut(){
+    let valiedAttendance2 = await global.db.query('SELECT * FROM attendance WHERE date=CURRENT_DATE AND  emp_id=? AND status="in"',[id]);
+      
+    if(!valiedAttendance2.length)
+      return true;
+      
+
+  }
+
 });
+
 
 module.exports = router;
